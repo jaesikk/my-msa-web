5cd672a4194cd10dfb5e70562c677ced19c6461d
jaesikk <jasic0627@gmail.com>
2022-12-20 21:01:31 +0900

    BE/user logic_ing

--- server/src/main/java/com/example/mymsaweb/user/userController.java
+++ server/src/main/java/com/example/mymsaweb/user/userController.java
@@ -1,15 +1,35 @@
 package com.example.mymsaweb.user;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.mymsaweb.domain.ResponseDto;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("api/user")
 public class userController {
+  // github, notion 참고해서 dto, builder 지향, pw hash
   @GetMapping("")
+  public ResponseDto<userDto> getUser(@RequestHeader(value="User-Id") String userId) {
+    User user = userService.findOne(userId);
+    return new ResponseDto(
+        HttpStatus.OK.value(),
+        "유저 조회",
+        new userDto(user.getId(), user.getAvatar(), user.getNickname()));
+  }
 
-
+  // 유저 생성 & 수정
   @PostMapping("")
+  public ResponseDto<userDto> createUser(@Valid @RequestBody UserCreateDto data) {
+    User user = userService.join(data.getId(), data.getAvatar(), data.getNickname());
+    return new ResponseDto(
+        HttpStatus.OK.value(),
+        "유저 생성",
+        new userDto(user.getId(), user.getAvatar(), user.getNickname()));
+  }
+
+  // 유저 삭제
+  @DeleteMapping("")
+  public void deleteUser(@RequestHeader(value="User-Id") String userId) {
+    userService.remove(userId);
+  }
 }
--- server/src/main/java/com/example/mymsaweb/user/userDto.java
+++ server/src/main/java/com/example/mymsaweb/user/userDto.java
@@ -1,4 +1,25 @@
 package com.example.mymsaweb.user;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
+@Entity @Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class userDto {
+  private Long id;
+  @NotEmpty(message = "아이디를 적어주세요.")
+  private String userId;
+  @Size(min = 1, max = 16, message = "이름은 최대 16자입니다.")
+  @NotEmpty(message = "이름을 적어주세요.")
+  private String userNm;
+  private String userPw;
+  private String userPlanId;
 }
